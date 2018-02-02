95f0796e7cc5c952aa933fc2d3ad79c317f28a3a
Spencer Whitehead <spencerwhitehead7@gmail.com>
2018-02-02 16:10:31 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,8 +4,8 @@ def fizzbuzz(number)
   if (n_by_three == n_by_three.floor) && (n_by_five == n_by_five.floor)
     return "FizzBuzz"
   elsif n_by_three == n_by_three.floor
-    puts "fizz"
+    return "fizz"
   elsif n_by_five == n_by_five.floor
-    puts "buzz"
+    return "uzz"
   end
 end
